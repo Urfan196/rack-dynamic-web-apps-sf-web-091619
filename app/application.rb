634d4b6f48cbc201f84318e8e0b944b606510f71
Urfan Guliyev <urfan196@yahoo.com>
2019-10-06 03:07:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
 
   def call(env)
-    resp = Rack::Response.newresp = Rack::Response.new
+    resp = Rack::Response.new
 
     num_1 = Kernel.rand(1..2)
     num_2 = Kernel.rand(1..2)
